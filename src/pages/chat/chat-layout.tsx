a3d690642651de8e5ea313b74f58edbaa7f19f37
Tarang Lakhmapurkar <tarangjlakh@gmail.com>
2025-02-25 08:40:30 +0530

    code fix

--- src/pages/chat/chat-layout.tsx
+++ src/pages/chat/chat-layout.tsx
@@ -1,13 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { useState } from "react";
 import ChatInterface from "./chat-page";
 import Sidebar from "./components/sidebar";
 
 const ChatLayout: React.FC = () => {
-    // const [sidebarOpen, setSidebarOpen] = useState<boolean>(true);
-
-
-
     return (
         <div className="flex h-screen bg-gray-100">
             <Sidebar />
