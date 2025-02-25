f1fa717c656b6caf3d6d01857acf12b402198d1f
Vyom Vasava <124495683+iamVyom12@users.noreply.github.com>
2025-02-25 16:30:05 +0530

    Update api.tsx

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,1 +1,48 @@
+import axios from 'axios';
 
+export const api = axios.create({
+  baseURL: 'https://stocksense-c7qv.onrender.com',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
+});
+
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('refreshToken');
+        window.location.href = '/login';
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
